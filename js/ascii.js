f7c0ec40f3f99844bb611cbb526b344f93797578
Zelalem Mekuria <zolaman231@gmail.com>
2021-05-25 17:11:56 -0500

    fix on the asccii.html page

--- js/ascii.js
+++ js/ascii.js
@@ -7,50 +7,50 @@ let frames;
 
 window.onload = function () {
     // disable stop button => 
-    document.getElementById('stopAnim').disabled = true;
+    document.getElementById("stopAnim").disabled = true;
 
     // when start button clicked
-    let start = document.getElementById('startAnim');
+    let start = document.getElementById("startAnim");
     start.onchange = startAnim;
 
     // when stop button clicked
-    let stop = document.getElementById('stopAnim');
+    let stop = document.getElementById("stopAnim");
     stop.onclick = stopAnimation;
 
     //when animation change
-    let anim = document.getElementById('animationType');
+    let anim = document.getElementById("animationType");
     anim.onchange = changeAnimation;
     anim.disabled = false;
 
     // when speed changed
-    let speed = document.getElementById('speed');
+    let speed = document.getElementById("speed");
     speed.onchange = speedChange;
 }
 
 function startAnim() {
     // starting animation
-    let animationType = document.getElementById('animationType');
+    let animationType = document.getElementById("animationType");
     let content = ANIMATIONS[animationType.value];
-    let textBox = document.getElementById('animationBox');
+    let textBox = document.getElementById("animationBox");
 
     text = textBox.value;
 
     frames = content.split("=====\n");
 
-    document.getElementById('startAnim').disabled = true;
-    document.getElementById('stopAnim').disabled = false;
+    document.getElementById("startAnim").disabled = true;
+    document.getElementById("stopAnim").disabled = false;
     animationType.disabled = true;
     animReference = setInterval(animateFrame, defaultSpeed, frames); // need modificaiton because it is not supported in IE9 and erlier versions
 }
 
 function animateFrame(input) {
-    let animationBox = document.getElementById('animationBox');
+    let animationBox = document.getElementById("animationBox");
     animationBox.value = frames[(temp % input.length)];
     temp++;
 }
 
 function speedChange() {
-    const speed = document.getElementById('speed').checked;
+    const speed = document.getElementById("speed").checked;
 
     if (speed) {
         defaultSpeed = 50;
@@ -64,28 +64,29 @@ function speedChange() {
 }
 
 function stopAnimation() {
-    let animationBox = document.getElementById('animationBox');
+    let animationBox = document.getElementById("animationBox");
     animationBox.value = text;
 
     text = '';
     clearInterval(animReference);
     temp = 0;
-    document.getElementById('startAnim').disabled = false;
-    document.getElementById('stopAnim').disabled = true;
-    document.getElementById('animationType').disabled = false;
+    document.getElementById("startAnim").disabled = false;
+    document.getElementById("stopAnim").disabled = true;
+    document.getElementById("animationType").disabled = false;
 
 }
 
 function changeAnimation() {
-    let animType = document.getElementById('animationType').value;
-    if (animType === 'Blank') {
-        console.log('set the text area empty value');
+    let animType = document.getElementById("animationType").value;
+    if (animType === "Blank") {
+        console.log("set the text area empty value");
     } else {
-        document.getElementById('animationBox').innerHTML = ANIMATIONS[animType];
+        document.getElementById("animationBox").innerHTML =
+            ANIMATIONS[animType];
     }
 }
 
 function changeFont() {
-    const selectedFontSize = document.getElementById('fontSizeType').value;
-    document.getElementById('animationBox').style.fontSize = selectedFontSize;
+    const selectedFontSize = document.getElementById("fontSizeType").value;
+    document.getElementById("animationBox").style.fontSize = selectedFontSize;
 }
